ef7a8beddbe41b6346b4b153ac7e07fefafd3c47
liam <liam.son@dktechin.com>
2024-09-12 08:36:03 +0900

    tt

--- src/component/contactItems/ContactItems.jsx
+++ src/component/contactItems/ContactItems.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 
 const ContactItems = ({ item }) => {
+  const [modal, setModal] = useState(false);
   return (
-    <div className="contact-items-wrap">
-      <div className="contact-items">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
-          alt="user-image"
-          width={40}
-        />
+    <div className="contact-items-contain">
+      <div className="contact-items-wrap">
+        <div className="contact-items">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png"
+            alt="user-image"
+            width={40}
+          />
+          <div>
+            <p>{item.name}</p>
+            <p>{item.phoneNumber}</p>
+          </div>
+        </div>
         <div>
-          <p>{item.name}</p>
-          <p>{item.phoneNumber}</p>
+          <p>
+            <FontAwesomeIcon icon={faEllipsis} className="see-more" />
+          </p>
         </div>
       </div>
-      <div>
-        <p>
-          <FontAwesomeIcon icon={faEllipsis} className="see-more" />
-        </p>
-      </div>
+      <Modal />
     </div>
   );
 };
 
+function Modal() {
+  return (
+    <div className="seemore-contain">
+      <div className="seemore-wrap">
+        <div>수정하기</div>
+        <div>삭제하기</div>
+      </div>
+    </div>
+  );
+}
 export default ContactItems;
